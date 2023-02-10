aa59bea3807eab3e4dcb0757f98881dfa20bc4a5
andreichemeris <crchemeris@gmail.com>
2023-02-10 07:37:01 -0600

    Filter and sorting in PDF report works

--- src/components/TableBalance.vue
+++ src/components/TableBalance.vue
@@ -15,8 +15,7 @@
         <thead>
           <tr>
             <th @click="sortBy('corrected.date')"><a class="waves-effect waves-orange pointer">{{
-              'Date_payment' |
-                localize
+              'Date_payment' | localize
             }}</a></th>
             <th @click="sortBy('corrected.correctionValue')"><a class="waves-effect waves-orange pointer">{{
               'Payment_amount' | localize
@@ -41,7 +40,7 @@
             <td :style="{ color: balance.corrected.correctionValue < 0 ? 'red' : 'inherit' }">
               {{ balance.corrected.correctionValue | currency }}
             </td>
-            <td> {{ balance.corrected.typePayment | localize }}</td>
+            <td> {{ balance.corrected.typePayment }}</td>
             <td>{{ balance.corrected.whosePayment }}</td>
             <td>{{ balance.corrected.reasonAdjustment }}</td>
             <td>{{ balance.corrected.nameAdmin }}</td>
@@ -57,6 +56,7 @@ import _ from 'lodash'
 import JsPDF from 'jspdf'
 import 'jspdf-autotable'
 import formatDate from '../../filters/date.filter'
+import localizeFilter from '../../filters/localize.filter'
 
 export default {
   name: 'TableBalance',
@@ -73,7 +73,7 @@ export default {
   mounted () {
     this.$store.dispatch('fetchAllData').then(dataBalance => {
       this.dataBalance = dataBalance.map(balance => {
-        balance.corrected.typePayment = balance.corrected.nameStudent ? 'student_fee' : 'Correct_balance'
+        balance.corrected.typePayment = localizeFilter(balance.corrected.nameStudent ? 'student_fee' : 'Correct_balance')
         balance.corrected.whosePayment = balance.corrected.nameStudent ? balance.corrected.nameStudent : balance.corrected.nameAdmin + ' (Admin)'
         return balance
       })
@@ -94,21 +94,19 @@ export default {
       this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc'
     },
     generatePDF () {
-      const originalDates = this.filteredData.map(p => p.corrected.date)
+      const originalDates = this.sortedData.map(p => p.corrected.date)
       const pdf = new JsPDF()
-
-      this.filteredData.forEach((balance) => {
+      this.sortedData.forEach((balance) => {
         balance.corrected.date = formatDate(balance.corrected.date)
       })
-
       pdf.autoTable({
-        head: [['Date Payment', 'Payment Amount', 'Type of Payment', 'Whose Payment', 'Commentary', 'Author of Record']],
-        body: this.filteredData.map(balance => [balance.corrected.date, balance.corrected.correctionValue, balance.corrected.typePayment, balance.corrected.whosePayment, balance.corrected.reasonAdjustment, balance.corrected.nameAdmin])
+        head: [
+          [localizeFilter('Date_payment'), localizeFilter('Payment_amount'), localizeFilter('Type_of_payment'), localizeFilter('Whose_payment'), localizeFilter('Commentary'), localizeFilter('author_of_record')]
+        ],
+        body: this.sortedData.map(balance => [balance.corrected.date, balance.corrected.correctionValue, balance.corrected.typePayment, balance.corrected.whosePayment, balance.corrected.reasonAdjustment, balance.corrected.nameAdmin])
       })
-
       pdf.save('table.pdf')
-
-      this.filteredData.forEach((balance, index) => {
+      this.sortedData.forEach((balance, index) => {
         balance.corrected.date = originalDates[index]
       })
     }
